5db68d17bbcf6f505582010945fc799005c76e39
alfredo <alfaradayepez@gmail.com>
2024-04-17 23:38:26 -0500

    there's no need

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'tu_base_de_datos');
-define('DB_USER', 'tu_usuario');
-define('DB_PASSWORD', 'tu_contraseña');
-
-echo 'lee el archivo incluido';
