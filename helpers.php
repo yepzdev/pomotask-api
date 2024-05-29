940e0ced76d6c5692401c3b42261e91c0655e182
alfredo <alfaradayepez@gmail.com>
2024-05-28 21:59:58 -0500

    add: utility file

---
+++ helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+function setHeaders() {
+    // Configure headers to allow CORS (Cross-Origin Resource Sharing)
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+}
